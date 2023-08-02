c38eaab1fdcc95dfcaeadbfef8f2814c841a9bb4
ryuu523 <ryunosuke20050523@outlook.jp>
2023-08-02 12:26:45 +0000

    update: update chat system and game system

---
+++ app.js
@@ -0,0 +1,164 @@
+const express = require('express')
+const app = express()
+const http = require('http').Server(app)
+const io = require('socket.io')(http)
+const PORT = process.env.PORT || 7000
+console.log(__dirname);
+const rootPath = __dirname + "/public"
+// const rootPath = "/home/public"
+const waitingPlayers = []; // マッチング待ちのプレイヤーを格納する配列
+const activeRooms = {}; // アクティブなルームを格納するオブジェクト
+app.use(express.static(rootPath))
+app.get("/", (req, res) => {
+  res.sendFile(rootPath + "/index.html")
+})
+
+io.on('connection', (socket) => {
+  // 接続時の処理
+  console.log('ユーザーが接続しました');
+
+  // プレイヤーがマッチング待ちに入る
+  waitingPlayers.push(socket);
+  if (waitingPlayers.length >= 2) {
+    const p1 = waitingPlayers.shift()
+    const p2 = waitingPlayers.shift()
+    const roomName = generateRoomName();
+    p1.join(roomName)
+    p2.join(roomName);
+    activeRooms[roomName] = {}
+    activeRooms[roomName]["players"] = [p1, p2]
+    activeRooms[roomName]["turn"] = Math.floor(Math.random() * 2)
+    const colors = ['rgb(255, 63, 63)', 'rgb(255, 63, 255)', 'rgb(255, 255, 63)', 'rgb(63, 255, 63)', 'rgb(63, 63, 255)', 'rgb(63, 255, 255)']
+    const answer = []
+    // activeRooms[roomName]["rand"] = [String(Math.floor(Math.random() * 10)), String(Math.floor(Math.random() * 10)), String(Math.floor(Math.random() * 10)), String(Math.floor(Math.random() * 10))]
+
+
+    for (let i = 0; i < 4; i++) {
+      answer.push(colors[Math.floor(Math.random() * colors.length)])
+    }
+    console.log(answer);
+    activeRooms[roomName]["answer"] = answer
+    console.log("マッチング成功:" + roomName);
+    io.to(roomName).emit("msg", "マッチング成功")
+    const parentPlayer = activeRooms[roomName]["players"][activeRooms[roomName]["turn"]]
+    const anotherPlayer = activeRooms[roomName]["players"][(1 - activeRooms[roomName]["turn"])]
+    parentPlayer.emit("msg", "あなたが先行です",1)
+    anotherPlayer.emit("msg", "あなたが後攻です",0)
+    parentPlayer.emit("turnCount", 1)
+    anotherPlayer.emit("turnCount", 0)
+  }
+  socket.on("msg", (data,you_or_other) => {
+    for (const roomName in activeRooms) {
+      const roomPlayers = activeRooms[roomName]["players"]
+      const perent=roomPlayers[activeRooms[roomName]["turn"]]
+      const another=roomPlayers[1-activeRooms[roomName]["turn"]]
+      const playerIndex = roomPlayers.indexOf(socket);
+      if (playerIndex == -1) {
+        continue
+      }
+      // perent.emit("talk",data,1)
+      // another.emit("talk",data,0)
+      io.to(roomName).emit("talk", data,you_or_other)
+      break
+    }
+  })
+  socket.on("nextturn", (data) => {
+    for (const roomName in activeRooms) {
+      const roomPlayers = activeRooms[roomName]["players"]
+      const playerIndex = roomPlayers.indexOf(socket);
+      if (playerIndex == -1) {
+        continue
+      }
+      activeRooms[roomName]["turn"] = 1 - activeRooms[roomName]["turn"]
+      const pPlayer = activeRooms[roomName]["players"][activeRooms[roomName]["turn"]]
+      const aPlayer = activeRooms[roomName]["players"][(1 - activeRooms[roomName]["turn"])]
+      pPlayer.emit("turnCount", 1)
+      aPlayer.emit("turnCount", 0)
+      break
+    }
+  })
+  socket.on("colors", (data) => {
+    for (const roomName in activeRooms) {
+      let hitnum = 0, blownum = 0
+      let anscopy = activeRooms[roomName]["answer"].concat()
+      const roomPlayers = activeRooms[roomName]["players"]
+      const playerIndex = roomPlayers.indexOf(socket);
+      if (playerIndex == -1) {
+        continue
+      }
+      data.forEach((color, index) => {
+        if (color === activeRooms[roomName]["answer"][index]) {
+          hitnum++
+          anscopy.forEach((a, index, array) => {
+            if (a === color) {
+              array[index] = "aa"
+            }
+          })
+        }
+      });
+      data.forEach((color, index) => {
+        if (anscopy.filter((item, i) => i !== index).includes(color)) {
+          blownum++
+          anscopy.forEach((a, index, array) => {
+            if (a == color) {
+              array[index] = "aa"
+            }
+          })
+        }
+      })
+      // console.log(hitnum);
+      // console.log(blownum);
+      // console.log(roomName);
+      io.to(roomName).emit("colordata", data)
+      io.to(roomName).emit("hitblow", hitnum, blownum)
+      // io.to(roomName).emit("blow",blownum)
+      break
+    }
+
+
+  })
+  socket.on("bingo", (data) => {
+    for (const roomName in activeRooms) {
+      let anscopy = activeRooms[roomName]["answer"].concat()
+      const roomPlayers = activeRooms[roomName]["players"]
+      const playerIndex = roomPlayers.indexOf(socket);
+      if (playerIndex == -1) {
+        continue
+      }
+      activeRooms[roomName]["players"][1-activeRooms[roomName]["turn"]].emit("v-or-d","YOUWIN")
+      activeRooms[roomName]["players"][activeRooms[roomName]["turn"]].emit("v-or-d","YOULOSE")
+      io.to(roomName).emit("bingo", anscopy)
+    }
+  })
+  // プレイヤーが切断した場合の処理
+  socket.on('disconnect', () => {
+    // マッチング待ちのプレイヤーから削除
+    const index = waitingPlayers.indexOf(socket);
+    if (index !== -1) {
+      waitingPlayers.splice(index, 1);
+    }
+
+    // アクティブなルームから削除
+    for (const roomName in activeRooms) {
+      const roomPlayers = activeRooms[roomName]["players"];
+      const pIndex = roomPlayers.indexOf(socket);
+      if (pIndex !== -1) {
+        roomPlayers.splice(pIndex, 1);
+        const anotherPlayer = roomPlayers[0]
+        anotherPlayer.emit("break", "切断されました")
+        delete activeRooms[roomName]; // ルームが空になったら削除
+        anotherPlayer.leave(roomName)
+        waitingPlayers.push(anotherPlayer)
+        break;
+      }
+    }
+
+    console.log('ユーザーが切断しました');
+  });
+  function generateRoomName() {
+    return `room-${Math.floor(Math.random() * 1000)}`;
+  }
+})
+http.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
---
+++ public/index.js
@@ -0,0 +1,247 @@
+const socket = io()
+const colors = ['rgb(255, 63, 63)', 'rgb(255, 63, 255)', 'rgb(255, 255, 63)', 'rgb(63, 255, 63)', 'rgb(63, 63, 255)', 'rgb(63, 255, 255)']
+const txt = document.getElementById("txt")
+const btn2 = document.getElementById("btn2")
+const colorpalet = document.querySelector('.colorpalet')
+const ballslast = document.querySelector('.ballslast')
+const boxslast = document.querySelector('.boxslast')
+const boxs = Array.from(document.getElementsByClassName('boxs'))
+const btn = Array.from(document.getElementsByClassName('btn'))
+const hb = document.querySelector('.hb')
+const turntext = document.querySelector(".turntext")
+const _sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const talkarea = document.querySelector(".talkarea")
+let colorNum
+let turnflag
+let answer
+let turn = -1
+let colorselement
+let resetcount = 0
+let you_or_other
+//時間置くやつ
+async function wait(ms) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
+}
+
+//イニシャライズ
+function init() {
+    turn = -1
+    if (resetcount == 0) {
+        for (let i = 0; i < 4; i++) {
+            let el = document.createElement('div')
+            el.innerHTML = "？"
+            el.classList.add('balllast')
+            ballslast.appendChild(el)
+            resetcount++
+        }
+    }
+    hb.classList.add("hide")
+
+}
+
+//色のパレット生成
+colors.forEach((color, index) => {
+    let el = document.createElement('div')
+    el.style.backgroundColor = color
+    el.classList.add('color');
+    el.addEventListener('click', () => {
+        const colorsdiv = document.querySelectorAll('.colorpalet > .color')
+        colorsdiv.forEach((colorElement) => {
+            colorElement.style.borderColor = '#252525';
+        })
+        el.style.borderColor = 'orange'
+        colorNum = index
+    })
+    colorpalet.appendChild(el)
+})
+//チャット打つところ
+txt.addEventListener("keydown", (e) => {
+    if (e.key == "Enter") {
+        socket.emit("msg", txt.value,you_or_other)
+        txt.value = ""
+    }
+})
+btn2.addEventListener("click", () => {
+    if (txt.value == "") {
+        return
+    }
+    socket.emit("msg", txt.value,you_or_other)
+    txt.value = ""
+})
+//切断された時の処理
+socket.on("break", (data) => {
+    reset()
+    const pp = Array.from(document.querySelectorAll(".talkarea>p"))
+    for (let i = 0; i < pp.length; i++) {
+        talkarea.lastChild.remove()
+    }
+    const p = document.createElement("p")
+    p.textContent = "切断されました"
+    p.style.color = "blue"
+    p.style.fontWeight = "bold"
+    talkarea.appendChild(p)
+})
+//ターン切り替え
+socket.on("turnCount", (data) => {
+    if (data == 1) {
+        turntext.textContent = "YOURTURN"
+        turnflag = false
+        turn++
+    }
+    else {
+        turntext.textContent = "ENEMYTURN"
+        turnflag = true
+        turn++
+    }
+
+})
+//マッチング成功の文字生成
+socket.on("msg", (data,id) => {
+    const p = document.createElement("p")
+    p.textContent = data
+    p.style.color = "blue"
+    p.style.fontWeight = "bold"
+    talkarea.appendChild(p)
+    you_or_other=id
+    console.log(id);
+
+})
+//チャット
+socket.on("talk", (data,id) => {
+    console.log(id);
+    const p = document.createElement("p")
+    if(you_or_other==id){
+        data="you:"+data
+    }
+    else{
+        data="enemy:"+data
+    }
+    p.textContent = data
+    talkarea.appendChild(p)
+})
+socket.on("colordata", (data) => {
+    data.forEach((color, index) => {
+        let balls = Array.from(document.querySelectorAll("#b" + (turn) + ">.balls>.ball"))
+        balls[index].style.backgroundColor = color
+    })
+})
+socket.on("hitblow", (hitnum, blownum) => {
+    let hnum = 0, bnum = 0
+    const box = boxs[turn - 1]
+    const pins = box.getElementsByClassName('pin')
+    for (let i = 0; i < hitnum; i++) {
+        setTimeout(() => {
+            pins[i].style.backgroundColor = "red";
+        }, 300 * hnum);
+        hnum++
+    }
+    for (let i = hitnum; i < blownum + hitnum; i++) {
+        setTimeout(() => {
+            pins[i].style.backgroundColor = "white";
+
+        }, 300 * (bnum + hnum));
+        bnum++
+    }
+    blowhitdraw(hnum, bnum)
+})
+socket.on("bingo", (data) => {
+    let el = document.getElementsByClassName("balllast")
+    data.forEach((color, index) => {
+        el[index].innerHTML = ""
+        el[index].style.backgroundColor = color
+        el[index].classList.add("fadein")
+    })
+})
+socket.on("v-or-d", (data) => {
+    turntext.textContent = data
+    turnflag = true
+})
+
+async function blowhitdraw(h, b) {
+    await wait((b + h + 2) * 300)
+    if (h == 4) {
+        hb.innerHTML = "4ヒット!!";
+        socket.emit("bingo")
+    }
+    else {
+        hb.innerHTML = h + "ヒット" + b + "ブロー";
+    }
+
+    hb.classList.remove("hide")
+    await wait(2000)
+    hb.classList.add("hide")
+
+
+}
+//丸を塗ってOKボタン出す系の処理
+boxs.forEach((box, index) => {
+    const blocks = Array.from(box.getElementsByClassName("ball"))
+    blocks.forEach((el) => {
+        el.addEventListener('click', () => {
+            if (!turnflag) {
+                if (turn == index) {
+                    el.style.backgroundColor = colors[colorNum]
+                    el.classList.add("selected")
+                    if (blocks.every(color => color.classList.contains("selected"))) {
+                        btn.forEach((b, ind) => {
+                            if (index === ind) {
+                                b.classList.remove('hide')
+                            }
+                        })
+                    }
+                }
+
+            }
+
+        })
+    })
+})
+//OKボタン押したときの動作
+async function OK() {
+    btn.forEach(b => {
+        b.classList.add('hide')
+    })
+    socket.emit("nextturn")
+    let ballsColor = Array.from(document.querySelectorAll("#b" + (turn + 1) + ">.balls>.ball"))
+    let arraycolors = []
+    ballsColor.forEach((ele) => {
+        arraycolors.push(ele.style.backgroundColor)
+    })
+    socket.emit("colors", arraycolors)
+}
+function reset() {
+    init()
+    boxs.forEach((box, index) => {
+        const blocks = Array.from(box.getElementsByClassName("ball"))
+        const pins = Array.from(box.getElementsByClassName('pin'))
+        pins.forEach((pin) => {
+            pin.style.backgroundColor = "rgb(14, 14, 14)"
+        })
+        blocks.forEach((el) => {
+
+            el.style.backgroundColor = "rgb(14, 14, 14)"
+            el.classList.remove("selected")
+
+        })
+    })
+    hb.innerHTML = " "
+    hb.style.color = "white"
+    hb.style.fontSize = "40px"
+    hb.classList.add("hide")
+    btn.forEach(b => {
+        b.classList.add('hide')
+    })
+    let balllast = Array.from(document.getElementsByClassName("balllast"))
+
+    balllast.forEach(el => {
+        el.innerHTML = "？"
+        el.style.backgroundColor = "rgb(14, 14, 14)"
+    })
+
+}
+
+init()
